62185e8973b068d4b794a6266b1a9d43cda86634
csi0n <huaqing.chen@bioon.com>
2017-05-23 17:40:38 +0800

    套接字连接稳定性调整

--- js/background.js
+++ js/background.js
@@ -29,8 +29,23 @@ function sendMessage(str, callback) {
     wsl.send(str);
 }
 var wsl = 'ws://localhost:8991'
-wsl = new WebSocket(wsl);
-wsl.onopen = function() {};
-wsl.onmessage = function(evt) {
-    console.log(evt);
-};
+var socket = checkWebSocket(wsl);
+setInterval(function() {
+    if (typeof socket === 'undefined' || socket.readyState === 3) {
+        var wsl = 'ws://localhost:8991'
+        socket = checkWebSocket(wsl);
+    }
+}, 10000);
+
+function checkWebSocket(url) {
+    wsl = new WebSocket(url);
+    wsl.onopen = function() {
+        wsl.onmessage = function(evt) {
+            console.log(evt);
+        };
+        wsl.onerror = function(evt) {
+            console.log(evt);
+        };
+    };
+    return wsl;
+}
